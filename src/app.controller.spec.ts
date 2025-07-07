9b49da253db01f2d2ef60158f17a2dee09eff9a5
joojodontoh <joojodontoh@airasia.com>
2025-07-07 17:24:39 +0800

    new

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World - this is Nest.Js!');
+      expect(appController.getHello()).toBe('Hello World - this is Nest.Js 0!');
     });
   });
 });
